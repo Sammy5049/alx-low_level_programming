74de0b4b467ff2b4d07b8274ab71a72a2d2a22d2
Damilola Adeola <samadedammy@gmail.com>
2023-06-18 07:37:00 -0700

    More added

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	int i;
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
-	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
-		putchar(APL[i]);
+		putchar(ALP[i]);
 	}
 	putchar('\n');
 	return (0);
