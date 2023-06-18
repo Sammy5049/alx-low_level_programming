5e232f7041d40c235cf2cebd07f608257b7fa8ac
Damilola Adeola <samadedammy@gmail.com>
2023-06-18 11:20:30 -0700

    Corrected

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,17 +13,17 @@ int main(void)
 
 	for (i = 48; i <= 56; i++)
 	{
-		for (j = 49; j <= 57; i++)
+		for (j = 49; j <= 57; j++)
 		{
 			if (j > i)
 			{
 				putchar(i);
 				putchar(j);
-			}
 			if (i != 56 || j != 57)
 			{
 				putchar(',');
 				putchar(' ');
+			}
 			}
 		}
 	}
