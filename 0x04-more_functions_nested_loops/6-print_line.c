5a89d206d7472dd066c9a89556067a4ffa39e2d7
Damilola Adeola <samadedammy@gmail.com>
2023-06-23 16:54:09 -0700

    Draw straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
@@ -18,8 +18,8 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+			putchar('_');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
