40dcbaff29cea951941cc7933fe250794c18eedb
Damilola Adeola <samadedammy@gmail.com>
2023-06-23 19:58:35 -0700

    Prime numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,20 @@
 
 void print_number(int n)
 {
-	unsigned int j = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		n *= -1;
-		j = n;
-		putchar('_');
+		n1 = -n;
+		putchar('-');
 	}
-	j /= 10;
-	if (j != 0)
-	print_number(j);
-	putchar((unsigned int) n % 10 + '0');
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
 }
