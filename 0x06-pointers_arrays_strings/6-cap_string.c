9c7943a805a98ada6febe0f43adc185d3df52326
Damilola Adeola <samadedammy@gmail.com>
2023-06-30 12:28:23 -0700

    Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * cap_string - a function that capitalize strings
+ * @str: the string to capitalize
+ * Return: return the string value
+ */
+
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+
+				str[i - 1] == '\t' ||
+
+				str[i - 1] == '\n' ||
+
+				str[i - 1] == ',' ||
+
+				str[i - 1] == ';' ||
+
+				str[i - 1] == '.' ||
+
+				str[i - 1] == '!' ||
+
+				str[i - 1] == '?' ||
+
+				str[i - 1] == '"' ||
+
+				str[i - 1] == '(' ||
+
+				str[i - 1] == ')' ||
+
+				str[i - 1] == '{' ||
+
+				str[i - 1] == '}' ||
+
+				i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
