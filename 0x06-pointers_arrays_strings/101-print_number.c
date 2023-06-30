c846d51c20297ee64dabb5cf8f32f21d95f80aa7
Damilola Adeola <samadedammy@gmail.com>
2023-06-30 15:37:34 -0700

    More pointer, array and string

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+* print_number - print numbers chars
+* @n: integer params
+* Return: 0
+*/
+
+void print_number(int n)
+
+{
+
+	unsigned int p1;
+
+	p1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		p1 = -n;
+	}
+	if (p1 / 10 != 0)
+	{
+		print_number(p1 / 10);
+	}
+	_putchar((p1 % 10) + '0');
+}
