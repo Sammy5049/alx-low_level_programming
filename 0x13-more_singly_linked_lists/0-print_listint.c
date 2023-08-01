c9c1111cb98a07bbdfb957e478b1d157f9e710ce
Damilola <samadedammy@gmail.com>
2023-08-01 02:52:04 -0700

    Print all nodes of lists_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
-
+	size_t count = 0;
 	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
-		h = h->next;
+                h = h->next;	
 	}
 
 	return (count);
