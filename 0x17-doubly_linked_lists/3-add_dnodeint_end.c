9d5e7a710787d4750f285f298dfd4af7a40ed902
Adeola Damilola <samadedammy@gmail.com>
2023-09-15 01:47:37 +0100

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,29 +9,29 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *node_end;
-    dlistint_t *temp;
+        dlistint_t *node_end;
+        dlistint_t *temp;
 
-    node_end = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (node_end == NULL)
-    {
-        return (NULL);
-    }
-    node_end->n = n;
-    node_end->next = NULL;
-    temp = *head;
-
-    if (temp != NULL)
-    {
-        while (temp->next != NULL)
+        node_end = (dlistint_t *)malloc(sizeof(dlistint_t));
+        if (node_end == NULL)
         {
-            temp = temp->next;
+                return (NULL);
         }
-        temp->next = node_end;
-    }
-    else
-        *head = node_end;
-    node_end->prev = temp;
+        node_end->n = n;
+        node_end->next = NULL;
+        temp = *head;
 
-    return (node_end);
+        if (temp != NULL)
+        {
+                while (temp->next != NULL)
+                {
+                        temp = temp->next;
+                }
+                temp->next = node_end;
+        }
+        else
+                *head = node_end;
+        node_end->prev = temp;
+
+        return (node_end);
 }
