a16bd678a5eb48ba94459ea5815ca1f948e98b16
Adeola Damilola <samadedammy@gmail.com>
2023-09-15 04:08:55 +0100

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free the list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp_node;
+
+	if (head != NULL)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
