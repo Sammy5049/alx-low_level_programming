593303ca35a441d51dd0c7c1cb93c7dfeac4acda
Damilola <samadedammy@gmail.com>
2023-09-15 07:22:30 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 {
 	size_t count, count2;
 	size_t size, add;
-	char *letter = "A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt
-		OGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *letter = "A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt"
+		      "OGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
 	if (argc != 2)
