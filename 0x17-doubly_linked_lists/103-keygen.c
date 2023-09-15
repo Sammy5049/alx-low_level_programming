2b39cfc26c9dc7fa4180ccc43ada3d21a9b17c2b
Damilola <samadedammy@gmail.com>
2023-09-15 07:33:14 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,8 +12,7 @@ int main(int argc, char *argv[])
 {
 	size_t count, count2;
 	size_t size, add;
-	char *letter = "A-CHRDw87lNS0E9B2TibgpnMVys5Xzvt\n"
-		      "OGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *z = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
 	if (argc != 2)
@@ -22,24 +21,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	size = strlen(argv[1]);
-	p[0] = letter[(size ^ 59) & 63];
+	p[0] = z[(size ^ 59) & 63];
 	for (count = 0, add = 0; count < size; count++)
 		add += argv[1][count];
-	p[1] = letter[(add ^ 79) & 63];
+	p[1] = z[(add ^ 79) & 63];
 	for (count = 0, count2 = 1; count < size; count++)
 		count2 *= argv[1][count];
-	p[2] = letter[(count2 ^ 85) & 63];
+	p[2] = z[(count2 ^ 85) & 63];
 	for (count2 = argv[1][0], count = 0; count < size; count++)
 		if ((char)count2 <= argv[1][count])
 			count2 = argv[1][count];
 	srand(count2 ^ 14);
-	p[3] = letter[rand() & 63];
+	p[3] = z[rand() & 63];
 	for (count2 = 0, count = 0; count < size; count++)
 		count2 += argv[1][count] * argv[1][count];
-	p[4] = letter[(count2 ^ 239) & 63];
+	p[4] = z[(count2 ^ 239) & 63];
 	for (count2 = 0, count = 0; (char)count < argv[1][0]; count++)
 		count2 = rand();
-	p[5] = letter[(count2 ^ 229) & 63];
+	p[5] = z[(count2 ^ 229) & 63];
 	printf("%s\n", p);
 	return (0);
 }
