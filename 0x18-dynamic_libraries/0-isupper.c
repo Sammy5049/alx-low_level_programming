7e215223c761522abb647dd7f7e5cdb6ec5bba89
Damilola <samadedammy@gmail.com>
2023-09-18 17:47:15 -0700

    0x018-dynamiclib

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check if letter is uppercase
+ * @C: Number to be checked
+ * Return: 1 if tru and 0 if false
+ */
+
+
+int _isupper(int C)
+{
+
+	if (C >= 65 && C <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * _strcat - A function to concatenate two strings and prints out the result
+ * @dest: first character to concatenate
+ * @src: second character to concatenate
+ * Return: Return dest always
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
